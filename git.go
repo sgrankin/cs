ee4888a1fa8112ca1be18c22f7675e7c0ac746fb
Sergey Grankin <sgrankin@gmail.com>
2024-03-18 16:36:26 -0400

    Misc git improvements
    Move all git code into git.go.
    Ignore repos that start with '.' (as they're meant to be hidden).

--- git.go
+++ git.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
+	"io/fs"
 	"log"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+	gitconf "github.com/go-git/go-git/v5/config"
 	gitplumb "github.com/go-git/go-git/v5/plumbing"
+	gitobj "github.com/go-git/go-git/v5/plumbing/object"
+	gittrans "github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/google/go-github/v57/github"
 )
 
@@ -39,6 +45,9 @@ func ResolveFetchSpecs(client *github.Client, specs []GitHubSourceConfig, auth *
 		ref := "HEAD"
 		if spec.Ref != "" {
 			ref = spec.Ref
+		}
+		if (*repo.Name)[0] == '.' {
+			return // Hidden repo, and technically an invalid refspec.  Ignore.
 		}
 		if (repo.Archived != nil && *repo.Archived && !spec.Archived) ||
 			(repo.Fork != nil && *repo.Fork && !spec.Forks) {
@@ -126,18 +135,83 @@ func GCRefs(repo *git.Repository, keep []string) error {
 	return nil
 }
 
-func ListRefs(repo *git.Repository) []string {
-	ri, err := repo.References()
-	if err != nil {
-		log.Panicf("listing refs: %v", err)
+type gitRepo struct {
+	repo      *git.Repository
+	remoteURL string
+	remoteRef string
+	localRef  string
+}
+
+func (r *gitRepo) Name() string { return r.localRef }
+
+func (r *gitRepo) Version() Version {
+	ref := r.ref()
+	if ref == nil {
+		return ""
 	}
-	defer ri.Close()
-	var refs []string
-	ri.ForEach(func(r *gitplumb.Reference) error {
-		if r.Type() == gitplumb.HashReference {
-			refs = append(refs, r.Name().Short())
-		}
+	return ref.Hash().String()
+}
+
+func (r *gitRepo) Files(yield func(RepoFile) error) error {
+	ref := r.ref()
+	if ref == nil {
+		// A missing ref has no files.
+		// This is not an error as this is also the case when the repo is simply empty.
 		return nil
+	}
+	commit, err := r.repo.CommitObject(ref.Hash())
+	if err != nil {
+		return fmt.Errorf("commit not found: %v", err)
+	}
+	tree, err := commit.Tree()
+	if err != nil {
+		return fmt.Errorf("tree not found: %v", err)
+	}
+	return tree.Files().ForEach(func(f *gitobj.File) error {
+		return yield(gitRepoFile{f})
 	})
-	return refs
+}
+
+func (r *gitRepo) Refresh() (Version, error) {
+	spec := gitconf.RefSpec("+" + r.remoteRef + ":refs/" + r.localRef)
+	log.Printf("fetching %q -> %q", r.Name(), spec)
+	err := git.NewRemote(r.repo.Storer, &gitconf.RemoteConfig{
+		URLs: []string{r.remoteURL},
+	}).Fetch(&git.FetchOptions{
+		RefSpecs: []gitconf.RefSpec{spec},
+		Depth:    1,
+		Progress: os.Stdout,
+	})
+	switch {
+	case err == nil:
+	case errors.Is(err, git.NoErrAlreadyUpToDate):
+	case errors.Is(err, gittrans.ErrEmptyRemoteRepository):
+	default:
+		return "", fmt.Errorf("fetching: %v", err)
+	}
+	return r.Version(), nil
+}
+
+func (r *gitRepo) ref() *gitplumb.Reference {
+	ref, err := r.repo.Reference(gitplumb.ReferenceName("refs/"+r.localRef), true)
+	if err != nil {
+		log.Printf("Missing reference %q in git repo %v: %v", r.localRef, r.repo, err)
+		return nil
+	}
+	return ref
+}
+
+type gitRepoFile struct{ f *gitobj.File }
+
+func (f gitRepoFile) Path() string { return f.f.Name }
+func (f gitRepoFile) Reader() io.ReadCloser {
+	r, _ := f.f.Blob.Reader()
+	return r
+}
+func (f gitRepoFile) Size() int { return f.Size() }
+
+func (f gitRepoFile) FileMode() fs.FileMode {
+	// Ignoring err.  Submodules will return a zero mode.
+	m, _ := f.f.Mode.ToOSFileMode()
+	return m
 }
